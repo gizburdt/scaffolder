86daae2d8fc08203f9c82b859093e32c8ec0f945
Gijs Jorissen <gizburdt@gmail.com>
2013-04-27 19:53:44 +0200

    X-UA-Compatible

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width" />
-	<meta http-equiv="X-UA-Compatible" content="IE=9" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
