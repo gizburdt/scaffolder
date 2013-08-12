ec036a6ca25096bd2a5d52be1ea29254fbf7dca6
Gijs Jorissen <gizburdt@gmail.com>
2013-08-12 10:05:05 +0200

    Maximum scale

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
