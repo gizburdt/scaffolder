f02246487f50bc1db90aad978f22232cce7d626e
Gizburdt <gizburdt@gmail.com>
2013-11-06 16:15:07 +0100

    Font-awesome with components

--- functions.php
+++ functions.php
@@ -1,122 +1,136 @@
 <?php
 
-	error_reporting( E_ALL );
-	ini_set( 'display_errors', 1 );
+// Error reporting
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
 
-/*==================================================*/
-/* Setup
-/*==================================================*/
+// BLock direct access
+if( ! defined( 'ABSPATH' ) ) exit;
 
+if( ! class_exists( 'Scaffold' ) ) :
+
+	// Set content_width
 	if ( ! isset( $content_width ) )
 		$content_width = 640;
-	
-	add_action( 'after_setup_theme', 'scaffold_setup' );
 
-	function scaffold_setup()
+
+	class Scaffold
 	{
-		// Includes
-		include( 'includes/customize/customizer.php' );
+		function __construct()
+		{
+			add_action( 'sfter_theme_setup', 	array( &$this, 'setup_theme' ) );
+			add_action( 'init', 				array( &$this, 'includes' ) );
+			add_action( 'init', 				array( &$this, 'add_hooks' ) );
+			add_action( 'init', 				array( &$this, 'add_admin_hooks' ) );
+		}
 
-		include( 'includes/walkers/walker-menu.php' );
-		include( 'includes/walkers/walker-comment.php' );
+		function includes()
+		{
+			// Includes
+			include( 'includes/customize/customizer.php' );
 
-		include( 'includes/widgets/widget.php' );
+			include( 'includes/walkers/walker-menu.php' );
+			include( 'includes/walkers/walker-comment.php' );
 
-		// Textdomain
-		load_theme_textdomain( 'scaffold', get_template_directory() . '/languages' );
+			include( 'includes/widgets/widget.php' );
+		}
 
-		// Theme support
-		add_theme_support( 'menus' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'automatic-feed-links' );
-		add_theme_support( 'post-formats', array( /* 'aside', 'link', 'gallery', 'status', 'quote', 'image' */ ) );
-		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+		function setup_theme()
+		{
+			// Textdomain
+			load_theme_textdomain( 'scaffold', get_template_directory() . '/languages' );
 
-		// Image sizes	
-		// add_image_size( 'name', width, height, true );
+			// Theme support
+			add_theme_support( 'menus' );
+			add_theme_support( 'post-thumbnails' );
+			add_theme_support( 'automatic-feed-links' );
+			add_theme_support( 'post-formats', array( /* 'aside', 'link', 'gallery', 'status', 'quote', 'image' */ ) );
+			add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
 
-		// Editor
-		add_editor_style( 'assets/css/editor-style.css' );
+			// Image sizes	
+			// add_image_size( 'name', width, height, true );
 
-		// Menus
-		register_nav_menus( array(
-			'primary-menu'		=> __( 'Primary', 'scaffold' ),
-			'footer-menu'		=> __( 'Footer Menu', 'scaffold' )
-		) );
+			// Editor
+			add_editor_style( 'assets/css/editor-style.css' );
 
-		// Widgets
-		add_action( 'widgets_init', 		'scaffold_register_extra_sidebars' );
-		add_action( 'widgets_init', 		'scaffold_register_widgets' );
-		add_filter(' widget_text', 			'do_shortcode' );
+			// Menus
+			register_nav_menus( array(
+				'primary-menu'		=> __( 'Primary', 'scaffold' ),
+				'footer-menu'		=> __( 'Footer Menu', 'scaffold' )
+			) );
 
-		// Mail
-		add_filter( 'wp_mail_from', 		'scaffold_new_mail_from' );
-		add_filter( 'wp_mail_from_name', 	'scaffold_new_mail_from_name' );
+			// Widgets
+			add_action( 'widgets_init', 		'scaffold_register_extra_sidebars' );
+			add_action( 'widgets_init', 		'scaffold_register_widgets' );
+			add_filter(' widget_text', 			'do_shortcode' );
 
-		// Misc
-		// add_filter( 'use_default_gallery_style', '__return_false' );
+			// Mail
+			add_filter( 'wp_mail_from', 		'scaffold_new_mail_from' );
+			add_filter( 'wp_mail_from_name', 	'scaffold_new_mail_from_name' );
+
+			// Misc
+			// add_filter( 'use_default_gallery_style', '__return_false' );
+		}
+
+		function add_hooks()
+		{
+			// Headers, footers, body
+			add_filter( 'the_generator', 		'scaffold_remove_wp_version' );
+			add_filter( 'body_class', 			'scaffold_body_class' );
+			add_filter( 'wp_title', 			'scaffold_wp_title', 10, 2 );
+			add_action( 'wp_head', 				'scaffold_favicon' );
+
+			// Styles
+			add_action( 'wp_enqueue_scripts', 	'scaffold_deregister_styles' );
+			add_action( 'wp_enqueue_scripts', 	'scaffold_register_styles' );
+			add_action( 'wp_enqueue_scripts', 	'scaffold_enqueue_styles' );
+			add_action( 'wp_head', 				'scaffold_add_specific_styles' );
+			add_action( 'login_head', 			'scaffold_register_login_styles' );
+			
+			// Scripts
+			add_action( 'wp_enqueue_scripts', 	'scaffold_deregister_scripts' );
+			add_action( 'wp_enqueue_scripts', 	'scaffold_register_scripts' );
+			add_action( 'wp_enqueue_scripts', 	'scaffold_enqueue_scripts' );
+			
+			// Excerpt
+			add_filter( 'excerpt_length', 		'scaffold_excerpt_length' );
+			add_filter( 'excerpt_more', 		'scaffold_excerpt_more' );
+
+			// Menu
+			add_filter( 'wp_nav_menu_objects', 	'scaffold_add_extra_menu_classes' );
+			add_filter( 'nav_menu_css_class', 	'scaffold_fix_menu_class', 10, 2 );
+
+			// SEO
+			add_filter( 'wpseo_author_link', 	'scaffold_no_author_on_pages' );
+		}
+
+		function add_admin_hooks()
+		{
+			if( is_admin() )
+			{
+				// Styles
+				// add_action( 'admin_init', 			'scaffold_register_admin_styles' );
+				// add_action( 'admin_print_styles', 	'scaffold_enqueue_admin_styles' );
+				
+				// Scripts
+				// add_action( 'admin_init', 			'scaffold_register_admin_scripts' );
+				// add_action( 'admin_enqueue_scripts', 'scaffold_enqueue_admin_scripts' );
+				
+				// Admin menu
+				add_action( 'admin_menu', 			'scaffold_remove_menu_pages' );
+				
+				// Dashboard widgets
+				add_action( 'wp_dashboard_setup', 	'scaffold_remove_dashboard_widgets' );
+				add_action( 'wp_dashboard_setup', 	'scaffold_add_dashboard_widgets' );
+				
+				// User
+				add_filter( 'user_contactmethods', 	'scaffold_edit_contactmethods' );
+
+				// Misc
+				add_filter( 'mce_buttons', 			'scaffold_enable_more_buttons' );
+			}
+		}
 	}
-
-	
-/*==================================================*/
-/* Hooks
-/*==================================================*/
-	
-	if( ! is_admin() ) 
-	{
-		// Headers, footers, body
-		add_filter( 'the_generator', 		'scaffold_remove_wp_version' );
-		add_filter( 'body_class', 			'scaffold_body_class' );
-		add_filter( 'wp_title', 			'scaffold_wp_title', 10, 2 );
-		add_action( 'wp_head', 				'scaffold_favicon' );
-
-		// Styles
-		add_action( 'wp_enqueue_scripts', 	'scaffold_deregister_styles' );
-		add_action( 'wp_enqueue_scripts', 	'scaffold_register_styles' );
-		add_action( 'wp_enqueue_scripts', 	'scaffold_enqueue_styles' );
-		add_action( 'wp_head', 				'scaffold_add_specific_styles' );
-		add_action( 'login_head', 			'scaffold_register_login_styles' );
-		
-		// Scripts
-		add_action( 'wp_enqueue_scripts', 	'scaffold_deregister_scripts' );
-		add_action( 'wp_enqueue_scripts', 	'scaffold_register_scripts' );
-		add_action( 'wp_enqueue_scripts', 	'scaffold_enqueue_scripts' );
-		
-		// Excerpt
-		add_filter( 'excerpt_length', 		'scaffold_excerpt_length' );
-		add_filter( 'excerpt_more', 		'scaffold_excerpt_more' );
-
-		// Menu
-		add_filter( 'wp_nav_menu_objects', 	'scaffold_add_extra_menu_classes' );
-		add_filter( 'nav_menu_css_class', 	'scaffold_fix_menu_class', 10, 2 );
-
-		// SEO
-		add_filter( 'wpseo_author_link', 	'scaffold_no_author_on_pages' );
-	}
-	else
-	{
-		// Styles
-		// add_action( 'admin_init', 			'scaffold_register_admin_styles' );
-		// add_action( 'admin_print_styles', 	'scaffold_enqueue_admin_styles' );
-		
-		// Scripts
-		// add_action( 'admin_init', 			'scaffold_register_admin_scripts' );
-		// add_action( 'admin_enqueue_scripts', 'scaffold_enqueue_admin_scripts' );
-		
-		// Admin menu
-		add_action( 'admin_menu', 			'scaffold_remove_menu_pages' );
-		
-		// Dashboard widgets
-		add_action( 'wp_dashboard_setup', 	'scaffold_remove_dashboard_widgets' );
-		add_action( 'wp_dashboard_setup', 	'scaffold_add_dashboard_widgets' );
-		
-		// User
-		add_filter( 'user_contactmethods', 	'scaffold_edit_contactmethods' );
-
-		// Misc
-		add_filter( 'mce_buttons', 			'scaffold_enable_more_buttons' );
-	}
-
 
 /*==================================================*/
 /* Headers, footers, body
@@ -202,7 +216,7 @@
 	{
 		// Vendor
 		wp_register_style( 'bootstrap', get_template_directory_uri() . '/vendor/twbs/bootstrap/dist/css/bootstrap.min.css', '', '', 'screen' );
-		wp_register_style( 'font-awesome', get_template_directory_uri() . '/vendor/fortawesome/font-awesome/css/font-awesome.min.css', '', '', 'screen' );
+		wp_register_style( 'font-awesome', get_template_directory_uri() . '/vendor/components/font-awesome/css/font-awesome.min.css', '', '', 'screen' );
 		wp_register_style( 'fancybox', get_template_directory_uri() . '/vendor/fancyapps/fancybox/source/jquery.fancybox.css', '', '', 'screen' );
 		
 		// Theme
@@ -524,4 +538,7 @@
 	         echo '</div>';
 	     }
 	}
-	
+
+endif; // Endif class_exists check
+
+$scaffold = new Scaffold();
