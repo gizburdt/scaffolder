6a6b08f7af7f3f336ec24cee13a670e1846aee93
Gijs Jorissen <gijs@360ecommerce.nl>
2014-06-13 13:00:08 +0200

    h2 in search results

--- content.php
+++ content.php
@@ -1,13 +1,15 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header>
-
 	<?php if ( is_search() ) : ?>
+		<header class="entry-header">
+			<h2 class="entry-title"><?php the_title(); ?></h2>
+		</header>
 		<div class="entry-summary">
 			<?php the_excerpt(); ?>
 		</div>
 	<?php else : ?>
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'scaffold' ) ); ?>
 		</div>
