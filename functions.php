236a9d485f9ae908739f1bc2700f06f727f28c40
Gijs Jorissen <gijs@360ecommerce.nl>
2014-06-18 14:41:30 +0200

    Add bxslider style

--- functions.php
+++ functions.php
@@ -93,6 +93,7 @@ function scaffold_register_styles()
 {
 	// Vendor
 	wp_register_style( 'jquery-fancybox', SCAFFOLD_VENDOR_URL . '/fancybox/source/jquery.fancybox.css', '', '', 'screen' );
+	wp_register_style( 'jquery-bxslider', SCAFFOLD_VENDOR_URL . '/bxslider-4/jquery.bxslider.css', '', '', 'screen' );
 	
 	// Theme
 	wp_register_style( 'style', get_stylesheet_directory_uri() . '/style.css', '', '', 'screen' );
@@ -104,6 +105,7 @@ function scaffold_register_styles()
 function scaffold_enqueue_styles()
 {
 	wp_enqueue_style( 'jquery-fancybox' );
+	wp_enqueue_style( 'jquery-bxslider' );
 	wp_enqueue_style( 'style' );
 }
 
@@ -119,7 +121,7 @@ function scaffold_register_scripts()
 	wp_register_script( 'respond', SCAFFOLD_VENDOR_URL . '/respond/dest/respond.min.js', '', '', true );
 	wp_register_script( 'enquire', SCAFFOLD_VENDOR_URL . '/enquire/dist/enquire.min.js', '', '', true );
 	wp_register_script( 'jquery-caroufredsel', SCAFFOLD_VENDOR_URL . '/carouFredSel/jquery.carouFredSel-6.2.1-packed.js', array( 'jquery' ), '', true);
-	wp_register_script( 'jquery-bxslider', SCAFFOLD_VENDOR_URL . '/bxslider/jquery.bxSlider.min.js', array( 'jquery' ), '', true);
+	wp_register_script( 'jquery-bxslider', SCAFFOLD_VENDOR_URL . '/bxslider-4/jquery.bxslider.min.js', array( 'jquery' ), '', true);
 	wp_register_script( 'jquery-fancybox', SCAFFOLD_VENDOR_URL . '/fancybox/source/jquery.fancybox.pack.js', array( 'jquery' ), '', true);
 }
 
