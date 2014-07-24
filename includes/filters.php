0351daf1e91eb65b3a03c53d45ed10aa55ff8304
Gijs Jorissen <gijs@360ecommerce.nl>
2014-07-24 08:37:58 +0200

    Add extra browsers to filter

--- includes/filters.php
+++ includes/filters.php
@@ -13,14 +13,18 @@ add_filter( 'widget_text', 'do_shortcode' );
  */
 function scaffold_body_class( $classes ) 
 {
+    global $, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
     global $is_gecko, $is_IE, $is_opera, $is_safari, $is_chrome;  
 
     if( $is_gecko )      	$classes[] = 'gecko';  
     elseif( $is_opera )  	$classes[] = 'opera';  
-    elseif( $is_safari )	$classes[] = 'safari';  
-    elseif( $is_chrome )	$classes[] = 'chrome';  
+    elseif( $is_safari )	$classes[] = 'safari';
+    elseif( $is_lynx )      $classes[] = 'lynx';
+    elseif( $is_chrome )	$classes[] = 'chrome';
+    elseif( $is_NS4 )       $classes[] = 'ns4'; 
     elseif( $is_IE )		$classes[] = 'ie';  
     else               		$classes[] = 'unknown-browser';
+    if( $is_iphone )        $classes[] = 'iphone';
 
     if( is_singular() ) {
     	global $post;
