9dc816984b640d6b35ffbf0ff336ff4897e24813
Gijs Jorissen <gijs@360ecommerce.nl>
2014-12-10 16:14:03 +0100

    Theme support for title-tag

--- functions.php
+++ functions.php
@@ -48,6 +48,7 @@ function scaffold_setup()
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-formats', array( /* 'aside', 'link', 'gallery', 'status', 'quote', 'image' */ ) );
+	add_theme_support( 'title-tag' );
 	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
 
 	// Editor
