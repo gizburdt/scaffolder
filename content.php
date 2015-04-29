f0bffd999be29fb50368ed107b41b0836ff1d444
Gijs Jorissen <gijs@360ecommerce.nl>
2015-04-29 21:07:39 +0200

    Small fix for archive

--- content.php
+++ content.php
@@ -3,18 +3,8 @@
 		<h2 class="entry-title"><?php the_title(); ?></h2>
 	</header>
 
-	<?php if ( is_search() ) : ?>
-
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-			<a href="<?php the_permalink(); ?>"><?php _e('Read more', 'scaffold') ?></a>
-		</div>
-
-	<?php else : ?>
-
-		<div class="entry-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'scaffold' ) ); ?>
-		</div>
-
-	<?php endif; ?>
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>"><?php _e('Read more', 'scaffold') ?></a>
+    </div>
 </div>
