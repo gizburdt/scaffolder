ed9476f247ba523a25dc569d873363580dcb5736
Gijs Jorissen <gizburdt@gmail.com>
2015-11-13 17:20:19 +0100

    Fix double h1 in archive title

--- archive.php
+++ archive.php
@@ -6,9 +6,7 @@
             <?php if ( have_posts() ) : ?>
 
                 <header class="page-header">
-                    <h1 class="page-title">
-                        <?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-                    </h1>
+                    <?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
                 </header>
 
                 <?php the_archive_description( '<div class="page-description">', '</div>' ); ?>
