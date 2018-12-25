febcb99a1db3c40087732f77170c745beb833c5e
Gijs Jorissen <mail@gijsjorissen.nl>
2018-12-25 20:06:20 +0100

    Docblock

--- app/admin.php
+++ app/admin.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+/**
+ * Add buttons to the editor
+ */
 add_filter('mce_buttons', function ($buttons) {
     if (! in_array('hr', $buttons)) {
         return array_add($buttons, 'hr', 'hr');
